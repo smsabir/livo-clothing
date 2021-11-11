9e8baa4d2dcc09afced754258e92e9c4084f7a83
S M Abdullah Al Sabir <ssabir@ttu.ee>
2021-11-11 15:41:51 +0200

    splited homepage to components and menu styled

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,51 +1,11 @@
 import React from 'react';
+import Directory from '../directory/directory';
 import "./Home.scss";
 
 const Home = () => {
     return (
         <div className="homepage">
-            <div className="directory-menu">
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">
-                            HATS
-                        </h1>
-                        <span className="subtitle">Shop Now</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">
-                            HATS
-                        </h1>
-                        <span className="subtitle">Shop Now</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">
-                            HATS
-                        </h1>
-                        <span className="subtitle">Shop Now</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">
-                            HATS
-                        </h1>
-                        <span className="subtitle">Shop Now</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">
-                            HATS
-                        </h1>
-                        <span className="subtitle">Shop Now</span>
-                    </div>
-                </div>
-            </div>
+            <Directory></Directory>
         </div>
     );
 };
